7209bf889e306c6c5dfc75bfc5926bdbba71b93e
steve.shangguan <steve.shangguan@e7fa9360-1be2-11de-8b5c-13accb87f508>
2009-05-10 19:03:23 +0000

    implementation of translator for team NcsaPc3 (UIUC).

--- src/edu/rpi/tw/provenance/pc3/converters/Ncsa.java
+++ src/edu/rpi/tw/provenance/pc3/converters/Ncsa.java
@@ -1,5 +1,166 @@
 package edu.rpi.tw.provenance.pc3.converters;
 
-public class Ncsa {
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.HashMap;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+
+import org.xml.sax.helpers.DefaultHandler;
+
+public class Ncsa extends DefaultHandler{
+	static private Writer out;
+
+	private String tempVal = "";
+
+	private HashMap<String, String> hp = new HashMap<String, String>();
+
+	public Ncsa() {
+
+		// initialize the mapping into a hashmap
+		hp.put("b:Account", "Context");
+		hp.put("b:Artifact", "Variable");
+		hp.put("b:CSV_file", "Variable");
+		hp.put("b:Process", "Function");
+		hp.put("a:Loop_Iteration", "Function");
+		hp.put("b:Role", "ProvRole");
+		hp.put("b:Generated", "WGB");
+		hp.put("b:Triggered", "WTB");
+		hp.put("b:Used", "Usd");
+		hp.put("c:timeInterval", "TimeSlice");
+
+		hp.put("b:eventAccount", "hasContext");
+		hp.put("b:generatedArtifact", "wgbTarget");
+		hp.put("b:generatedByProcess", "wgbSource");
+		hp.put("b:triggeredProcess", "wtbTarget");
+		hp.put("b:triggeredByProcess", "wtbSource");
+		hp.put("b:usedByProcess", "usdSource");
+		hp.put("b:usedArtifact", "usdTarget");
+		hp.put("b:usedRole", "hasRole");
+		hp.put("b:generatedRole", "hasRole");
+		hp.put("c:usedTime", "hasTimeSlice");
+		hp.put("c:noEarlier", "hasStartTime");
+		hp.put("c:noLater", "hasStopTime");
+		hp.put("a:PathToFile", "hasValue");
+
+		hp.put("rdf:type", "hasType");
+		hp.put("rdfs:label", "rdfs:label");
+		hp.put("rdf:RDF", "rdf:RDF");
+
+	}
+
+	/**
+	 * Parse the RDF/XML document
+	 */
+	public void parseDocument() {
+
+		// get a factory
+		SAXParserFactory spf = SAXParserFactory.newInstance();
+
+		try {
+
+			// get a new instance of parser
+			SAXParser sp = spf.newSAXParser();
+
+			// parse the file and also register this class for call backs
+			sp.parse(
+					"E:\\Research-RPI\\tw\\provenance\\2009-04_2009-05_pc3\\team\\NcsaPc3\\output.rdf",
+					this);
+
+		} catch (SAXException se) {
+			se.printStackTrace();
+		} catch (ParserConfigurationException pce) {
+			pce.printStackTrace();
+		} catch (IOException ie) {
+			ie.printStackTrace();
+		}
+	}
+
+	/**
+	 * Write the parsed string into ProvProto ontology file
+	 */
+	private void printData(String s) {
+		try {
+			out.write(s);
+			out.flush();
+		} catch (IOException e) {
+			System.out.println("IO Error!");
+		}
+	}
+
+	private void newLine() {
+		String lineEnd = System.getProperty("line.separator");
+		try {
+			out.write(lineEnd);
+		} catch (IOException e) {
+			System.out.println("IO Error!");
+		}
+	}
+
+	// Event Handlers
+	public void startDocument() throws SAXException {
+		printData("<?xml version='1.0' encoding='UTF-8'?>");
+		newLine();
+	}
+
+	public void endDocument() throws SAXException {
+		try {
+			newLine();
+			out.flush();
+		} catch (IOException e) {
+			throw new SAXException("I/O error", e);
+		}
+	}
+
+	public void startElement(String uri, String localName, String qName,
+			Attributes attributes) throws SAXException {
+		if (hp.containsKey(qName)) {
+			printData("<" + hp.get(qName));
+			if (attributes != null) {
+				for (int i = 0; i < attributes.getLength(); i++) {
+					printData(" ");
+					printData(attributes.getQName(i) + "=\""
+							+ attributes.getValue(i) + "\"");
+				}
+			}
+			printData(">");
+		} else {
+			System.out.println("No matching defined.");
+		}
+
+	}
+
+	public void characters(char[] ch, int start, int length)
+			throws SAXException {
+		tempVal = new String(ch, start, length);
+		printData(tempVal);
+	}
+
+	public void endElement(String uri, String localName, String qName)
+			throws SAXException {
+
+		if (hp.containsKey(qName)) {
+			printData("</" + hp.get(qName) + ">");
+		} else {
+			System.out.println("No matching defined.");
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// Set up output stream
+		try {
+			out = new OutputStreamWriter(System.out, "UTF8");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		Ncsa nc3 = new Ncsa();
+		nc3.parseDocument();
+	}
 }
